c6406af05eed12ad1488b94094b46405fa595649
pankaj <pankaj7187815@gmail.com>
2024-01-20 14:02:55 +0530

    I C 20-01-2024 02.02 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,TypeaheadModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
